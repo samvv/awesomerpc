4ebeb395328ebbf78566510f83ecaf20f3d3f8b7
Sam Vervaeck <samvv@pm.me>
2026-03-18 18:04:10 +0100

    Also export `TransportError` from package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-export { type Transport, WebSocketTransport, DummyTransport, RawTransport, createDuplex,  } from "./transport.js"
+export { type Transport, WebSocketTransport, DummyTransport, RawTransport, createDuplex, TransportError } from "./transport.js"
 export { implement, contract, emptyContract, anyContract } from "./types.js"
 export { RPC, type AnyMethods, type AnyEvents, connect } from "./rpc.js"
 export { createProxy } from "./proxy.js"
